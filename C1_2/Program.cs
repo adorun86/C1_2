c58a9bedd895dfce50d9400cd9ac5b2de23caa19
adorun86 <51911403+adorun86@users.noreply.github.com>
2019-07-05 00:13:26 +0500

    решено 1 задание

--- C1_2/Program.cs
+++ C1_2/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C1_2
+namespace C1_2_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-        }sdadas
+            Console.Write("Введите 1 число : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите 2 число : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите 3 число: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int min;
+
+            if (a < b && a<c)
+            {
+                min = a;
+            }
+            else
+            {
+                if (b < c)
+                {
+                    min = b;
+                }
+                else
+                {
+                    min = c;
+                }
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+
+        }
     }
 }
