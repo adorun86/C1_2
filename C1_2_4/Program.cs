a325b0ec04baa02169b2f007521ba21fe773444e
adorun86 <51911403+adorun86@users.noreply.github.com>
2019-07-05 01:43:10 +0500

    Сделанно задание 3 и 4

---
+++ C1_2_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loggin = "root";
+            string password = "GeekBrains";
+            int tryCount = 3;
+
+            do
+            {
+                Console.WriteLine("Введите логин");
+                string tryGuessLog = Console.ReadLine();
+                Console.WriteLine("Введите пороль");
+                string tryGuessPass = Console.ReadLine();
+                if (tryGuessLog == loggin && tryGuessPass == password)
+                {
+                    Console.WriteLine("Добро пожаловать");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Логин или пороль не верен");
+                    
+
+                }
+                
+                tryCount--;
+                if (tryCount > 0)
+                {
+                    Console.WriteLine("Осталось " + tryCount +" попыток");
+                }
+            } while (tryCount > 0);
+            
+        }
+    }
+}
