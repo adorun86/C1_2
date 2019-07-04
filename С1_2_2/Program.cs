27e39aadddbf547a10b2c75db1c03cda09bf16aa
adorun86 <51911403+adorun86@users.noreply.github.com>
2019-07-05 00:23:01 +0500

    сделано задание 2

---
+++ С1_2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С1_2_2
+{
+    //2. Написать метод подсчета количества цифр числа.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            string message = Console.ReadLine();
+
+            Console.Write(message.Length);
+            Console.ReadKey();
+        }
+    }
+}
