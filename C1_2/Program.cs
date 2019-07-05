b0767f95c845b168ab8794963826cb9a988d1490
adorun86 <51911403+adorun86@users.noreply.github.com>
2019-07-05 18:25:43 +0500

    1

--- C1_2/Program.cs
+++ C1_2/Program.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace C1_2_1
-{
-    // Машканцев 
-    //1. Написать метод, возвращающий минимальное из трех чисел.
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите 1 число : ");
-            int a = Convert.ToInt32(Console.ReadLine());
+//namespace C1_2_1
+//{
+//    // Машканцев 
+//    //1. Написать метод, возвращающий минимальное из трех чисел.
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write("Введите 1 число : ");
+//            int a = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите 2 число : ");
-            int b = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Введите 2 число : ");
+//            int b = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите 3 число: ");
-            int c = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Введите 3 число: ");
+//            int c = Convert.ToInt32(Console.ReadLine());
 
-            int min;
+//            int min;
 
-            if (a < b && a < c)
-            {
-                min = a;
-            }
-            else
-            {
-                if (b < c)
-                {
-                    min = b;
-                }
-                else
-                {
-                    min = c;
-                }
-            }
-            Console.WriteLine(min);
-            Console.ReadKey();
+//            if (a < b && a < c)
+//            {
+//                min = a;
+//            }
+//            else
+//            {
+//                if (b < c)
+//                {
+//                    min = b;
+//                }
+//                else
+//                {
+//                    min = c;
+//                }
+//            }
+//            Console.WriteLine(min);
+//            Console.ReadKey();
 
-        }
-    }
-}
+//        }
+//    }
+//}
--- С1_2_2/Program.cs
+++ С1_2_2/Program.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace С1_2_2
-{
-    // Mashkantsev
-    //2. Написать метод подсчета количества цифр числа.
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите число: ");
-            string message = Console.ReadLine();
+//namespace С1_2_2
+//{
+//    // Mashkantsev
+//    //2. Написать метод подсчета количества цифр числа.
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write("Введите число: ");
+//            string message = Console.ReadLine();
 
-            Console.Write(message.Length);
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.Write(message.Length);
+//            Console.ReadKey();
+//        }
+//    }
+//}
