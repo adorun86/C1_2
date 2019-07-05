2a8bee0f09ff6fcabebdd5fa5fcf12db6301a71f
adorun86 <51911403+adorun86@users.noreply.github.com>
2019-07-05 18:26:13 +0500

    1

--- C1_2_4/Program.cs
+++ C1_2_4/Program.cs
@@ -1,49 +1,49 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C1_2_4
-//{
-//    class Program
-//    {
-//        //Mashkantsev
-//        //4. Реализовать метод проверки логина и пароля. На вход подается логин и пароль. 
-//        //На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains).
-//        //Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, программа 
-//        //пропускает его дальше или не пропускает. С помощью цикла do while ограничить ввод пароля тремя попытками.
-//        static void Main(string[] args)
-//        {
-//            string loggin = "root";
-//            string password = "GeekBrains";
-//            int tryCount = 3;
+namespace C1_2_4
+{
+    class Program
+    {
+        //Mashkantsev
+        //4. Реализовать метод проверки логина и пароля. На вход подается логин и пароль. 
+        //На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains).
+        //Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, программа 
+        //пропускает его дальше или не пропускает. С помощью цикла do while ограничить ввод пароля тремя попытками.
+        static void Main(string[] args)
+        {
+            string loggin = "root";
+            string password = "GeekBrains";
+            int tryCount = 3;
 
-//            do
-//            {
-//                Console.WriteLine("Введите логин");
-//                string tryGuessLog = Console.ReadLine();
-//                Console.WriteLine("Введите пороль");
-//                string tryGuessPass = Console.ReadLine();
-//                if (tryGuessLog == loggin && tryGuessPass == password)
-//                {
-//                    Console.WriteLine("Добро пожаловать");
-//                    break;
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Логин или пороль не верен");
-                    
+            do
+            {
+                Console.WriteLine("Введите логин");
+                string tryGuessLog = Console.ReadLine();
+                Console.WriteLine("Введите пороль");
+                string tryGuessPass = Console.ReadLine();
+                if (tryGuessLog == loggin && tryGuessPass == password)
+                {
+                    Console.WriteLine("Добро пожаловать");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Логин или пороль не верен");
 
-//                }
-                
-//                tryCount--;
-//                if (tryCount > 0)
-//                {
-//                    Console.WriteLine("Осталось " + tryCount +" попыток");
-//                }
-//            } while (tryCount > 0);
-//            //dsa
-//        }
-//    }
-//}
+
+                }
+
+                tryCount--;
+                if (tryCount > 0)
+                {
+                    Console.WriteLine("Осталось " + tryCount + " попыток");
+                }
+            } while (tryCount > 0);
+            //dsa
+        }
+    }
+}
--- С1_2_3/Program.cs
+++ С1_2_3/Program.cs
@@ -1,39 +1,39 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace С1_2_3
-//{
-//    //Mashkantsev
-//    //3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int sum = 0;
-//            Console.WriteLine("Введите числа");
-//            while (true)
-//            {
-//                int inputNumber = Convert.ToInt32(Console.ReadLine());
-//                if (inputNumber != 0)
-//                {
-//                    if (inputNumber % 2 == 0)
-//                    {
-                        
-//                    }
-//                    else
-//                    {
-//                        sum = sum + inputNumber;
-//                    }
-//                }
-//                else
-//                {
-//                    break;
-//                }
-//            }
-//            Console.WriteLine(sum);
-//        }
-//    }
-//}
+namespace С1_2_3
+{
+    //Mashkantsev
+    //3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Введите числа");
+            while (true)
+            {
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
+                if (inputNumber != 0)
+                {
+                    if (inputNumber % 2 == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        sum = sum + inputNumber;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
